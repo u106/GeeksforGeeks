a22605315cb2766d0870c1fd5fa28b264cc743e6
Udbhav Prasad <136351402+u106@users.noreply.github.com>
2026-08-27 22:41:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bubble Sort/bubble-sort.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap adjacent elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no elements were swapped, array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+}
