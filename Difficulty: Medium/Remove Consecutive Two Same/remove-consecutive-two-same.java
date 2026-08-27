a7f7dd1ee8f3b4f8f4f1cf824271f2906a931eaa
Udbhav Prasad <136351402+u106@users.noreply.github.com>
2026-08-27 22:33:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove Consecutive Two Same/remove-consecutive-two-same.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String removePair(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            int len = sb.length();
+            if (len > 0 && sb.charAt(len - 1) == c) {
+                sb.deleteCharAt(len - 1);
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.length() == 0 ? "-1" : sb.toString();
+    }
+}
