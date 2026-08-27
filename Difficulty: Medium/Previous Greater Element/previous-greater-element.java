1fce0d702edf1330436bb8663a55296ce65f902f
Udbhav Prasad <136351402+u106@users.noreply.github.com>
2026-08-27 21:47:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Previous Greater Element/previous-greater-element.java
@@ -0,0 +1,33 @@
+class Solution {
+    public ArrayList<Integer> preGreaterEle(int[] arr) {
+        // code here
+        int n = arr.length;
+        ArrayList<Integer> res = new ArrayList<>();
+        Stack<Integer> stk = new Stack<>();
+
+        // Initialize res with -1 for all elements
+        for (int i = 0; i < n; i++) {
+            res.add(-1);
+        }
+
+        for (int i = 0; i<n; i++) {
+
+            // Pop elements from the stack that are less
+            // than or equal to the current element
+            while (!stk.isEmpty() && stk.peek() <= arr[i]) {
+                stk.pop();
+            }
+
+            // If the stack is not empty, the top element
+            // is the next greater element
+            if (!stk.isEmpty()) {
+                res.set(i, stk.peek());
+            }
+
+            // Push the current element onto the stack
+            stk.push(arr[i]);
+        }
+
+        return res;
+    }
+}
